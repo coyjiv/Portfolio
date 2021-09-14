fd891d3d78dae034ceb09f6bed755986d4e38930
Даниил Щербаков <djdaniil46@gmail.com>
2021-09-14 15:53:43 +0300

    cards ready (without links and button functionality)

--- src/Components/CardWrapper/CardWrapper.jsx
+++ src/Components/CardWrapper/CardWrapper.jsx
@@ -3,22 +3,85 @@ import styled from "styled-components";
 import Card from "../Card/Card";
 
 const CardWrapper = (props) => {
-    const CardC = styled.div`display:flex;`;
+    const CardC = styled.div`
+    padding-top:102px;
+    height: 1890px;
+     width:80%;
+      margin:0px auto;
+       display:flex;
+        flex-wrap:wrap;
+        flex-direction: column;
+         justify-content:flex-start;
+          align-content:flex-start;
+          align-items:flex-start;
+           gap:50px;
+           row-gap:208px`;
     return(
         <CardC>
-            <Card name="SERTIN" background="#201F1F;" color="#C4C4C4" fsize="80"
+            <Card name="SERTIN" color="#C4C4C4" fsize="80" infoSize="25" info="Landing Page with UI interactions. Non-adaptive."
                   addRules="
                   transform:translateY(26px)translateX(22px);
                   "
-                  cardRules="
-                  transform:translateY(102px)translateX(84px);
+                  cardRules="background: rgb(94,97,97);
+background: linear-gradient(0deg, rgba(94,97,97,1) 0%, rgba(32,31,31,1) 69%);box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
+
                   "/>
-                  <Card name="FORKIO" background="#201F1F;" color="#C4C4C4" fsize="80"
+                  <Card name="landing" color="#201F1F;" fsize="61" infoSize="30" info="Landing Page with UI interactions. Adaptive for most resolutions."
                         addRules="
                   transform:translateY(26px)translateX(22px);
                   "
-                        cardRules="
-                  transform:translateY(310px)translateX(300px);
+                        cardRules="background: rgb(168,168,168);
+background: linear-gradient(0deg, rgba(168,168,168,1) 0%, rgba(196,196,196,1) 100%);box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
+                  "/>
+                  <Card name="TWITTER POST" infoSize="30" info="Blog variation logic app. Adaptive for most resolutions." color="#C4C4C4" fsize="44"
+                        addRules="
+                  transform:translateY(26px)translateX(22px);
+                  "
+                        cardRules="background: rgb(94,97,97);
+background: linear-gradient(0deg, rgba(94,97,97,1) 0%, rgba(32,31,31,1) 69%);box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
+                  "/>
+                  <Card name="FORKIO" infoSize="30" info="The landing fully-adaptive project that I've made with my colleague Luka Ponomarenko a couple of months ago. Adaptive for most resolutions."color="#C4C4C4" fsize="80"
+                        addRules="
+                  transform:translateY(26px)translateX(22px);
+                  "
+                        cardRules="background: rgb(94,97,97);
+background: linear-gradient(0deg, rgba(94,97,97,1) 0%, rgba(32,31,31,1) 69%);box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
+                  "/>
+                  <Card name="OSCILLATOR" infoSize="25" info="Oscillator playground where you can set different soundwave
+frequency and discover how does the sound changes due to number of oscillators."background="#C4C4C4" color="#201F1F" fsize="53"
+                        addRules="
+                  transform:translateY(26px)translateX(22px);
+                  "
+                        cardRules="background: rgb(168,168,168);
+background: linear-gradient(0deg, rgba(168,168,168,1) 0%, rgba(196,196,196,1) 100%);box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
+                  "/>
+                  <Card name="REACTSHOP" infoSize="30" info="Small shop logic app that I've made using React. Adaptive for most resolutions."background="#201F1F" color="#C4C4C4" fsize="42"
+                        addRules="
+                  transform:translateY(26px)translateX(22px);
+                  "
+                        cardRules="background: rgb(94,97,97);
+background: linear-gradient(0deg, rgba(94,97,97,1) 0%, rgba(32,31,31,1) 69%);box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
+                  "/>
+            <Card name="THE HAM" infoSize="30" info="Landing Page with basic UI interactions. Adaptive for most resolutions."color="#C4C4C4" fsize="67"
+                  addRules="
+                  transform:translateY(26px)translateX(22px);
+                  "
+                  cardRules="background: rgb(94,97,97);
+background: linear-gradient(0deg, rgba(94,97,97,1) 0%, rgba(32,31,31,1) 69%);box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
+                  "/>
+            <Card name="ARCHITECTURE" infoSize="30" info="Landing Page with basic UI interactions. Adaptive for most resolutions."background="#C4C4C4" color="#201F1F" fsize="45"
+                  addRules="
+                  transform:translateY(26px)translateX(22px);
+                  "
+                  cardRules="background: rgb(168,168,168);
+background: linear-gradient(0deg, rgba(168,168,168,1) 0%, rgba(196,196,196,1) 100%);box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
+                  "/>
+            <Card name="doctor appointments" infoSize="25" info="This is a small app that we have created with my colleagues: Luka Ponomarenko and Vitalii Tabanets in pure vanilla JS. The goal was to create an app where the secretary could easily create visits for patients and write additional info."color="#C4C4C4" fsize="43"
+                  addRules="
+                  transform:translateY(26px)translateX(22px);
+                  "
+                  cardRules="background: rgb(94,97,97);
+background: linear-gradient(0deg, rgba(94,97,97,1) 0%, rgba(32,31,31,1) 69%);box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
                   "/>
         </CardC>
     );
--- src/Components/PortfolioCards/PortfolioCards.jsx
+++ src/Components/PortfolioCards/PortfolioCards.jsx
@@ -3,7 +3,18 @@ import styled from "styled-components";
 import CardWrapper from "../CardWrapper/CardWrapper";
 
 const PortfolioCards = (props) => {
-    const Portfolio = styled.section`height: 2000px;mix-blend-mode: difference; background-image:url(/portfolio.png);`;
+    const Portfolio = styled.section`height: 2000px;background-image:url(/portfolio.png); mix-blend-mode: difference;
+    &::before{
+    content:"";
+    display:block;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background:#000;
+    opacity:0.6;
+    z-index:-1;
+    }
+    `;
     return(
         <Portfolio>
             <CardWrapper/>
